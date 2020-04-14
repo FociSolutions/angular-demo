2bfb6e74b9e06e9ed918e3edd55211c3232e2a54
xhsun <xhsun@users.noreply.github.com>
2020-04-14 13:58:48 -0400

    clear gh-pages branch

--- src/app/unit-test/shared/models/echo-data.model.ts
+++
@@ -1,3 +0,0 @@
-export default interface EchoData {
-  data: string;
-}
--- src/app/web-storage/shared/models/key-value-pair.model.ts
+++
@@ -1,5 +0,0 @@
-// An interface defining the key value pair that will be returned from the storage api
-export default interface KeyValuePair {
-  key: string;
-  value: string;
-}
